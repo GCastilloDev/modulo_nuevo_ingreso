1cdc8b2caa45fbdf1a00f739c06508d24b89cab7
tavoGeek <gcnctz@gmail.com>
2020-08-16 13:50:37 -0500

    :gear: VUETIFJS :zap: Se configuro la fuente y los pesos de la misma.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+  <v-container>
+   
+  </v-container>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  components: {
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
